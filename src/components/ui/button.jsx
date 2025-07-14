281b6fbb93b49cd0678a06e6eae9b8a202bd1331
afrizal-10 <adijuliyantoafrizal10@gmail.com>
2025-07-14 11:42:52 +0700

    fix bug ui button

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {Slot} from "@radix-ui/react-slot";
-import {cn} from "@/lib/utils";
 import {buttonVariants} from "./button-variants";
+import {cn} from "@/lib/utils";
 
 const Button = React.forwardRef(
   ({className, variant, size, asChild = false, ...props}, ref) => {
